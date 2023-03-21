9283989a01eb61d956403c56a39529500f4b5c25
OstapZ <zherebetskyiq7@gmail.com>
2023-03-21 14:42:00 +0200

    Implemented: [200~

---
+++ tests/orders/conftest.py
@@ -0,0 +1,26 @@
+import random
+
+import pytest
+import allure
+
+
+@pytest.fixture
+def additional_args(random_product_from_db):
+    with allure.step(
+      f"Get a product id: {random_product_from_db['ID']}"
+    ):
+        product_id = random_product_from_db["ID"]
+
+    additional_args = {
+        "line_items": [
+            {
+                "product_id": product_id,
+                "quantity": random.randint(1, 10)
+            }
+        ]
+    }
+
+    with allure.step(
+      f"Get a line_items for additional args: {additional_args}"
+    ):
+        return additional_args
