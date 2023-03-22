b43a25ddfb1302ac8fa1f391aa751ad1159079e3
OstapZ <zherebetskyiq7@gmail.com>
2023-03-22 14:32:06 +0200

    Implemented DB_HOST for database connection for different machines

--- src/configs/hosts_config.py
+++ src/configs/hosts_config.py
@@ -5,5 +5,73 @@ API_HOST = {
 }
 
 DB_HOST = {
-
+    "machine1": {
+        "dev": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "stage": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "prod": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        }
+    },
+    "machine2": {
+        "dev": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "stage": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "prod": {
+            "host": "localhost",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        }
+    },
+    "docker": {
+        "dev": {
+            "host": "host.docker.internal",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "stage": {
+            "host": "host.docker.internal",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        },
+        "prod": {
+            "host": "host.docker.internal",
+            "database": "local",
+            "table_prefix": "wp_",
+            "socket": None,
+            "port": 10006
+        }
+    }
 }
