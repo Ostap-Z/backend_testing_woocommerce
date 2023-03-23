07c0f0728bad5d64647c8972360d5ee06a915d46
OstapZ <zherebetskyiq7@gmail.com>
2023-03-23 19:24:48 +0200

    Implemented test_update_order_status_with_invalid_data test case that verifies a message is correct in API response

--- tests/orders/test_update_order.py
+++ tests/orders/test_update_order.py
@@ -2,6 +2,8 @@ import allure
 import pytest
 
 from src.helpers.orders_helper import OrdersHelper
+from src.utilities.requests_utility import RequestsUtility
+from src.utilities.generator_utility import generate_random_string
 
 
 @allure.suite("Orders endpoint")
@@ -9,6 +11,7 @@ from src.helpers.orders_helper import OrdersHelper
 @pytest.mark.orders
 class TestUpdateOrder:
     orders_helper = OrdersHelper()
+    request_utility = RequestsUtility()
 
     @allure.title(
         "Verify that the created order status could be updated"
@@ -63,3 +66,59 @@ class TestUpdateOrder:
                 f"\n\tActual status: {updated_status}" \
                 "\nExpected result:" \
                 f"\n\tStatus should be updated with the {status} status"
+
+    @allure.title(
+        "Verify that the created order status could be updated"
+    )
+    @allure.severity(
+        severity_level=allure.severity_level.NORMAL
+    )
+    @pytest.mark.tcid55
+    def test_update_order_status_with_invalid_data(
+        self,
+        order
+    ):
+        with allure.step(
+          f"Get an order id: {order['id']}"
+        ):
+            order_id = order["id"]
+
+        with allure.step(
+            "Generate a random invalid status"
+        ):
+            invalid_status = generate_random_string()
+
+        payload = {
+            "status": invalid_status
+        }
+        expected_response_message = "Invalid parameter(s): status"
+
+        with allure.step(
+            "Update an order with invalid status: "
+            f"{payload['status']}"
+        ):
+            response = self.request_utility.put(
+                endpoint=f"orders/{order_id}",
+                payload=payload,
+                expected_status_code=400
+            )
+
+        with allure.step(
+            "Take a message from the API response: "
+            f"{response['message']}"
+        ):
+            response_message = response["message"]
+
+        with allure.step(
+            "Verify that a message in the API response "
+            f"equals to the {expected_response_message}"
+        ):
+            assert response_message == expected_response_message, \
+                "\nActual result:" \
+                "\n\tUpdate order status to random string " \
+                "has invalid message in API response" \
+                f"\n\tActual response code: {response_message}" \
+                f"\n\tActual response: {response}" \
+                "\nExpected result:" \
+                "\n\tMessage in API response " \
+                f"should be {expected_response_message}"
