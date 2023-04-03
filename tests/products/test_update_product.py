87e6667b5b1bc17666a658cbaafbda307013c445
OstapZ <zherebetskyiq7@gmail.com>
2023-04-03 16:31:25 +0300

    Implemented test_update_product_price testcase

--- tests/products/test_update_product.py
+++ tests/products/test_update_product.py
@@ -61,3 +61,56 @@ class TestUpdateProduct:
           f"with {regular_price_payload}"
         ):
             assert updated_price == regular_price_payload
+
+    @allure.title(
+        "Verify that the update 'sale_price > 0' sets 'on_sale'=True"
+    )
+    @allure.severity(
+        severity_level=allure.severity_level.CRITICAL
+    )
+    @pytest.mark.tcid58
+    def test_update_product_price(
+        self,
+        simple_product
+    ):
+        with allure.step(
+          f"Get the product id: {simple_product['id']}"
+        ):
+            product_id = simple_product["id"]
+
+        payload = {
+            "sale_price": "1"
+        }
+
+        with allure.step(
+          "Update the product with data: "
+          f"{product_id=}, {payload=}"
+        ):
+            self.products_helper.update_product(
+                product_id,
+                payload
+            )
+
+        with allure.step(
+            f"Get the updated product with id: {product_id}"
+        ):
+            updated_product = self.products_helper.get_product_by_id(product_id)
+
+        with allure.step(
+            "Get the on_sale status from updated product "
+            f"with id {product_id}: {updated_product['on_sale']}"
+        ):
+            product_on_sale = updated_product['on_sale']
+
+        with allure.step(
+            "Verify that the on_sale status is True"
+        ):
+            assert product_on_sale, \
+                "\nActual result:" \
+                "\n\t'on_sale' parameter is not True " \
+                "after updating with 'sale_price > 0'" \
+                f"\n\tActual 'on_sale' status: {product_on_sale}" \
+                f"\n\tPayload: {payload}" \
+                f"\nExpected result:" \
+                f"\n\t'on_sale' parameter should ne equaled to true " \
+                f"after updating with 'sale_price > 0'"
